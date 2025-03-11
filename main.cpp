414e48c2d77780e81d7d92c6cccf5f24271a6818
sergiorandria <167822166+sergiorandria@users.noreply.github.com>
2025-03-11 08:37:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int main()
     std::string str1 = "Rakontondraibe Aina";
     std::string str2 = "Rakotondrabe Aina";
 
-    auto gram1 = generateNGrams(str1, 6);
-    auto gram2 = generateNGrams(str2, 6);
+    auto gram1 = generateNGrams(str1, 3);
+    auto gram2 = generateNGrams(str2, 3);
     std::cout << JaccardIndex(gram1, gram2);
     return 0;
 }
